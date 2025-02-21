7d26dcec957139844f2a19cc74e05ed7fda0eef1
danquiggin <dquiggin1@gsu.edu>
2025-02-21 13:44:00 -0500

    Add files via upload

---
+++ eventssummer2025.js
@@ -0,0 +1,221 @@
+const scheduleData = {
+    "events": [
+        {
+            "title": "Basic Federal Taxation (see Note X in Overview Tab)",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2,
+                4
+            ],
+            "extendedProps": {
+                "instructor": "Yaun, E",
+                "room": "245",
+                "modality": "B",
+                "capacity": "50",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Different and Unusual Forms of I.P. (see Note X in Overview Tab)",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                1
+            ],
+            "extendedProps": {
+                "instructor": "Charney, R",
+                "room": "245",
+                "modality": "B",
+                "capacity": "40",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Education Law",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2
+            ],
+            "extendedProps": {
+                "instructor": "Scott, C",
+                "room": "345",
+                "modality": "B",
+                "capacity": "25",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Family Law",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2,
+                4
+            ],
+            "extendedProps": {
+                "instructor": "Clark-Wilson, C",
+                "room": "242",
+                "modality": "F2F",
+                "capacity": "50",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Real Estate Transactions (see Note X in Overview Tab)",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                1,
+                3
+            ],
+            "extendedProps": {
+                "instructor": "Gibbs, T",
+                "room": "345",
+                "modality": "B",
+                "capacity": "45",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Tax Clinic - Tax Court I",
+            "startTime": "02:00 PM",
+            "endTime": "04:45 PM",
+            "daysOfWeek": [
+                1,
+                3
+            ],
+            "extendedProps": {
+                "instructor": "Watts, C & Yaun, E",
+                "room": "244",
+                "modality": "F2F",
+                "capacity": "12",
+                "year": "",
+                "credit": "6"
+            }
+        },
+        {
+            "title": "Tax Clinic - Tax Court II",
+            "startTime": "02:00 PM",
+            "endTime": "04:45 PM",
+            "daysOfWeek": [
+                1,
+                3
+            ],
+            "extendedProps": {
+                "instructor": "Watts, C & Yaun, E",
+                "room": "244",
+                "modality": "F2F",
+                "capacity": "6",
+                "year": "",
+                "credit": "6"
+            }
+        },
+        {
+            "title": "Contract Drafting and Risk Analysis",
+            "startTime": "05:00 PM",
+            "endTime": "07:45 PM",
+            "daysOfWeek": [
+                1,
+                3
+            ],
+            "extendedProps": {
+                "instructor": "Weston, H",
+                "room": "346",
+                "modality": "F2F",
+                "capacity": "30",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Policy Surveillance & Legal Epidemiology (see Note X in Overview Tab) ",
+            "startTime": "05:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2,
+                4
+            ],
+            "extendedProps": {
+                "instructor": "Kershner, S & Johnston, K",
+                "room": "246",
+                "modality": "OS, OA",
+                "capacity": "20",
+                "year": "",
+                "credit": "3"
+            }
+        }
+    ],
+    "online_classes": [
+        {
+            "title": "Professional Responsibility",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2
+            ],
+            "extendedProps": {
+                "instructor": "Cole, A",
+                "room": "Online",
+                "modality": "OS",
+                "capacity": "35",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Computers and the Law Seminar",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                3
+            ],
+            "extendedProps": {
+                "instructor": "Sites, B",
+                "room": "Online",
+                "modality": "OS, WR",
+                "capacity": "15",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Lawyering: Practice Ready Writing",
+            "startTime": "06:00 PM",
+            "endTime": "08:45 AM",
+            "daysOfWeek": [
+                2
+            ],
+            "extendedProps": {
+                "instructor": "Vath, M",
+                "room": "Online",
+                "modality": "OS, WR",
+                "capacity": "12",
+                "year": "",
+                "credit": "3"
+            }
+        },
+        {
+            "title": "Externship Seminar",
+            "startTime": "04:10 PM",
+            "endTime": "05:50 PM",
+            "daysOfWeek": [
+                2
+            ],
+            "extendedProps": {
+                "instructor": "Silverthorne, B",
+                "room": "Online",
+                "modality": "OS",
+                "capacity": "20",
+                "year": "",
+                "credit": "2"
+            }
+        }
+    ]
+};
